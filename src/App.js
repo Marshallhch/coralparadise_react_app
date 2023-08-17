67744532099e3f5ca9d5b58d2a2643b9276a80e5
Marshallhch <marshallhch@gmail.com>
2023-08-17 16:49:47 +0900

    요청 테스트 완료

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from './pages/Home';
+import SearchLists from './pages/SearchLists';
+import Details from './pages/Details';
+
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+      <SearchLists />
+      <Details />
+    </div>
+  );
+}
+
+export default App;
